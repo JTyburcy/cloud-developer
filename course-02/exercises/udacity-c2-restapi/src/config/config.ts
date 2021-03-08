ef2242ca16df8a3f051703c041292ccef18b9bd8
JTyburcy <jtyburcy@gmail.com>
2021-03-08 23:25:46 +0900

    Finish Lesson 4.5

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "DBUdacityDev",
-    "password": "DBUdacityDev",
-    "database": "UdaCity",
-    "host": "dbudacitydev.cxw4ty8q9ipb.ap-northeast-1.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "ap-northeast-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "mynewbucket20210307"
+    "username": process.env.POSTGRESS_USERNAME2,
+    "password": process.env.POSTGRESS_PASSWORD2,
+    "database": process.env.POSTGRESS_DATABASE2,
+    "host": process.env.POSTGRESS_HOST2,
+    "dialect": process.env.AWS_DIALECT2,
+    "aws_region": process.env.AWS_REGIONS2,
+    "aws_profile": process.env.AWS_PROFILES2,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKETS2
   },
   "prod": {
     "username": "",
@@ -17,3 +17,4 @@ export const config = {
     "dialect": "postgres"
   }
 }
+
